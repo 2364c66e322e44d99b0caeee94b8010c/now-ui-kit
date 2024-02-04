42047048f8a2a80e7e194c6a0c6477274acdd98b
Marco Azimonti <git@azimonti.com>
2024-02-04 10:23:13 +0900

    completed the porting to Bootstrap 5, removed demo code from the main js and updated Readme to include Bootstrap 5 support

--- README.md
+++ README.md
@@ -4,7 +4,12 @@
 
 ![Product Image](https://s3.amazonaws.com/creativetim_bucket/products/56/original/opt_nuk_thumbnail.jpg?1466002372 "Now UI Kit")
 
-**[Now UI Kit](http://demos.creative-tim.com/now-ui-kit/index.html?ref=nuk-readme)** is a responsive Bootstrap 4 kit provided for free by Invision and Creative Tim. It is a beautiful cross-platform UI kit featuring over 50 elements and 3 templates.
+
+**Bootstrap 5 Support**
+Now UI Kit has been migrated to Bootstrap 5. This makes starting a new project very simple. It also provides benefits if you are already working on a Bootstrap 5 project; you can just import the Now UI Kit style over it. Most of the elements have been redesigned; but if you are using an element we have not touched, it will fall back to the Bootstrap default.
+
+
+**[Now UI Kit](http://demos.creative-tim.com/now-ui-kit/index.html?ref=nuk-readme)** is a responsive Bootstrap 4 kit provided for free by Invision and Creative Tim. It is a beautiful cross-platform UI kit featuring over 50 elements and 3 templates, and was migrated to be compatible with Bootstrap 5. Build process has also been updated with the latest version of Gulp.
 
 Now UI Kit is one of the most popular UI Kits online, provided in PSD and Sketch formats by Invision. We wanted to find a way for developers worldwide to benefit from using it. So, in collaboration with Invision, we are launching the HTML version for it!
 
@@ -14,9 +19,6 @@ Create awesome, lifelike prototypes with InVision and Now so your users can expe
 
 To get the PSD and Sketch files, please visit Invision (https://www.invisionapp.com/now).
 
-
-**Bootstrap 4 Support**
-Now UI Kit is built on top of the much awaited Bootstrap 4. This makes starting a new project very simple. It also provides benefits if you are already working on a Bootstrap 4 project; you can just import the Now UI Kit style over it. Most of the elements have been redesigned; but if you are using an element we have not touched, it will fall back to the Bootstrap default.
 
 **Example Pages**
 We wanted to fully display the power of this kit, so the kit comes packed with examples showing you how to use the components. Inside the product you will find:
---
+++ assets/js/demo.js
@@ -0,0 +1,131 @@
+/*!
+
+ =========================================================
+ * Now UI Kit - v1.3.1 - Demo
+ =========================================================
+
+ * Product Page: https://www.creative-tim.com/product/now-ui-kit
+ * Copyright 2019 Creative Tim (http://www.creative-tim.com)
+
+ * Designed by www.invisionapp.com Coded by www.creative-tim.com
+
+ =========================================================
+
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+ */
+
+
+var big_image;
+
+
+var nowuiKit, oVal;
+
+$(document).ready(function() {
+
+  if ($(window).width() >= 992) {
+    big_image = $('.page-header-image[data-parallax="true"]');
+
+    $(window).on('scroll', nowuiKitDemo.checkScrollForParallax);
+  }
+});
+
+// Javascript just for Demo purpose, remove it from your project
+nowuiKitDemo = {
+  checkScrollForParallax: debounce(function() {
+    var current_scroll = $(this).scrollTop();
+
+    oVal = ($(window).scrollTop() / 3);
+    big_image.css({
+      'transform': 'translate3d(0,' + oVal + 'px,0)',
+      '-webkit-transform': 'translate3d(0,' + oVal + 'px,0)',
+      '-ms-transform': 'translate3d(0,' + oVal + 'px,0)',
+      '-o-transform': 'translate3d(0,' + oVal + 'px,0)'
+    });
+
+  }, 6)
+
+}
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this,
+      args = arguments;
+    clearTimeout(timeout);
+    timeout = setTimeout(function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    }, wait);
+    if (immediate && !timeout) func.apply(context, args);
+  };
+};
+
+nowuiKit = {
+ 
+  initSliders: function() {
+    // Sliders for demo purpose in refine cards section
+    var slider = document.getElementById('sliderRegular');
+
+    noUiSlider.create(slider, {
+      start: 40,
+      connect: [true, false],
+      range: {
+        min: 0,
+        max: 100
+      }
+    });
+
+    var slider2 = document.getElementById('sliderDouble');
+
+    noUiSlider.create(slider2, {
+      start: [20, 60],
+      connect: true,
+      range: {
+        min: 0,
+        max: 100
+      }
+    });
+  }
+}
+
+// Javascript just for Demo purpose, remove it from your project
+nowuiKitDemo = {
+  checkScrollForParallax: debounce(function() {
+    var current_scroll = $(this).scrollTop();
+
+    oVal = ($(window).scrollTop() / 3);
+    big_image.css({
+      'transform': 'translate3d(0,' + oVal + 'px,0)',
+      '-webkit-transform': 'translate3d(0,' + oVal + 'px,0)',
+      '-ms-transform': 'translate3d(0,' + oVal + 'px,0)',
+      '-o-transform': 'translate3d(0,' + oVal + 'px,0)'
+    });
+
+  }, 6)
+
+}
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this,
+      args = arguments;
+    clearTimeout(timeout);
+    timeout = setTimeout(function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    }, wait);
+    if (immediate && !timeout) func.apply(context, args);
+  };
+};
